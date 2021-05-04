dbcfa04d8f3b23c8c7c5ff04e82b26ec84fa7ea1
Amal Derouich <64805748+amalDerouich@users.noreply.github.com>
2021-05-04 01:55:00 +0100

    tp-angular

---
+++ src/app/directives/ng-class/ng-class.component.spec.ts
@@ -0,0 +1,26 @@
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgClassComponent } from './ng-class.component';
+
+describe('NgClassComponent', () => {
+  let component: NgClassComponent;
+  let fixture: ComponentFixture<NgClassComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NgClassComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NgClassComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/random-age.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RandomAgePipe } from '@angular/cli/tpAngular/src/app/pipes/random-age.pipe';
+
+describe('RandomAgePipe', () => {
+  it('create an instance', () => {
+    const pipe = new RandomAgePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
